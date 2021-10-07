edac9c6f15e6858d57321d2c7c2ba1baab3bfcb8
Lauren Zugai <lauren@zugai.com>
2021-10-07 18:52:28 -0500

    Cleanup, alter readme, add trialRun optional arg

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 
 # po-translations-to-ftl
 
-Say your team is migrating from [gettext](https://www.npmjs.com/package/node-gettext) to [Fluent](https://www.npmjs.com/package/@fluent/bundle) and your l10n team already did the work to translate the strings, which exist in a `.po` file in multiple language directories. You've got your newly available `.ftl` file with strings, and you want to port over all the existing `.po` translations to an `.ftl` file in multiple language directories.
+This script was created to help the Firefox Accounts team programmatically migrate existing email translations from [gettext](https://www.npmjs.com/package/node-gettext) `.po` files to [Fluent]((https://www.npmjs.com/package/@fluent/bundle)) `.ftl` files. The original intent was to expand the functionality or make it easy to use elsewhere, but at the time of writing, changes were implemented that are likely specific to FxA / FxA emails.
 
-Does this oddly specific case apply to you?* Here's the tool for you!
+This tool currently assumes:
+* You have a single `.ftl` file with ftl IDs and strings
+* The `.po` file where translations will be extracted exist in an `LC_MESSAGES` directory within multiple language directories, e.g. `locale/ar/LC_MESSAGES/my-file.po` and `locale/es/LC_MESSAGES/my-file.po`
+* The strings set in your `.ftl` file match the `msgid` in your `.po` files
 
-<b>*NOTE:</b> This script in its current state is rudimentary and there's almost certainly unaccounted for edge cases. It was originally written for one ftl file and functionality can/should be expanded. Checkout TODO comments. This has only been tested on a Mac.
+Known things that won't transfer over properly since we haven't had a use case yet:
+* Lines beginning and ending with variables containing copy between, like `%(my-name)s is a cool %(description)s`
+* Nested strings with multiple `=` in `.ftl` files, such as:
+```
+avatar-your-avatar =
+  .alt = Your avatar
+```
+
+Use at your own risk, as outlined here this is pretty rudimentary and there's almost certainly unaccounted for edge cases.
+
+This has only been tested on a Mac.
 
 ## Installation
 
-Clone the repo and install:
+Simply clone the repo and install:
 
 ```
 npm install
@@ -17,6 +30,16 @@ npm install
 
 ## Usage
 
+Pass in arguments when you run `npm start`.
+
+Arguments:
+* `ftlDir` (required, string) - the path to the directory containing the ftl file to be referenced
+* `ftlFile` (required, string) - the name of the .ftl file to be referenced
+* `localeDir` (required, string) - the path to the locale directory containing language directories, which contain an LC_MESSAGES directory with .po files
+* `poFile` (required, string) - the name of the .po file to copy existing translations from
+* `trialRun` (optional, boolean) - if set to true, instead of writing all .ftl files to disk, console log the first two to verify they look as expected
+
+Example:
 ```
-npm start -- --ftlDir=../fxa/packages/fxa-auth-server/public/locales/en --ftlFile=auth.ftl --localeDir=../fxa-content-server-l10n/locale --poFile=server.po
+npm start -- --ftlDir=../fxa/packages/fxa-auth-server/public/locales/en --ftlFile=auth.ftl --localeDir=../fxa-content-server-l10n/locale --poFile=server.po --trialRun=true
 ```
--- index.ts
+++ index.ts
@@ -1,28 +1,33 @@
 
-import fs from "fs-extra"
+import fs from 'fs-extra'
 import yargs from 'yargs'
-import { hideBin } from "yargs/helpers"
+import { hideBin } from 'yargs/helpers'
 
-const { ftlDir, ftlFile, localeDir, poFile } = yargs(hideBin(process.argv)).options({
+const { ftlDir, ftlFile, localeDir, poFile, trialRun } = yargs(hideBin(process.argv)).options({
   ftlDir: {
     type: 'string', 
     demandOption: true,
-    describe: 'The path to the directory containing the ftl file to be copied',
+    describe: 'The path to the directory containing the ftl file to be referenced',
   },
   ftlFile: {
     type: 'string', 
     demandOption: true,
-    describe: 'The name of the .ftl file to be copied',
+    describe: 'The name of the .ftl file to be referenced',
   },
   poFile: {
     type: 'string', 
     demandOption: true,
-    describe: 'The name of the .po file to reference current translations',
+    describe: 'The name of the .po file to copy existing translations from',
   },
   localeDir: { 
     type: 'string', 
     demandOption: true,
     describe: 'The path to the locale directory containing language directories, which contain an LC_MESSAGES directory with .po files',
+  },
+  trialRun: { 
+    type: 'boolean', 
+    default: false,
+    describe: 'Instead of writing all .ftl files to disk, console log the first two to verify they look as expected',
   },
 }).parseSync()
 
@@ -64,7 +69,7 @@ const convertPoVarsToFltVars = (poContent: String) => {
     let convertedPoContent = poContent
     poFtlVarMap.forEach(({poVar, ftlVar}) => {
       poContent.split(poVar).join()
-      convertedPoContent = convertedPoContent.replace(poVar, ftlVar);
+      convertedPoContent = convertedPoContent.replace(poVar, ftlVar)
     })
     return convertedPoContent
   }
@@ -83,106 +88,120 @@ const convertPoVarsToFltVars = (poContent: String) => {
   return poContent
 }
 
+interface SetWithComments {
+  /** the single line or multi-line comment if one exists */
+  comment: String | null;
+  ftlId: string;
+  engTranslation: string;
+  /** comment + ftlId + engTranslation */
+  fullString: string
+}
+
+/**
+ * @param ftlConcatSetsWithComments Contains every single line (including each commented line) or multi-line block from the ftl file except blank lines.
+ */
+const getSetsWithComments = (ftlConcatSetsWithComments: string[]) => {
+  const setsWithComments: SetWithComments[] = []
+  let matchIndex = 0
+  ftlConcatSetsWithComments.forEach((match, index) => {
+    // don't iterate over match if it's been reached in the inner loop
+    if (matchIndex <= index) {
+      if (!match.startsWith('#')) {
+        // we can grab the fluent ID/message by using what's on the left/hand side of ' = ' 
+        // except for blocks where they may begin on the next line (' ='). Needs tweaking for 
+        // nested strings with multiple `=` 
+        const [ ftlId, engTranslation ] = match.includes(' = ') ? match.split(' = ') : match.split(' =')
+        setsWithComments.push({
+          comment: null,
+          ftlId,
+          engTranslation,
+          fullString: match,
+        })
+      } else {
+        let comment = match
+        for (let i = index + 1; i < ftlConcatSetsWithComments.length; i++) {
+          if (ftlConcatSetsWithComments[i].startsWith('#')) {
+            comment += '\n' + ftlConcatSetsWithComments[i]
+          } else {
+            const ftlIdAndTranslation = ftlConcatSetsWithComments[i]
+            const [ ftlId, engTranslation ] = ftlIdAndTranslation.includes(' = ') ? ftlIdAndTranslation.split(' = ') : match.split(' =')
+            setsWithComments.push({
+              comment,
+              ftlId,
+              engTranslation,
+              fullString: comment + '\n' + ftlIdAndTranslation,
+            })
+            break
+          }
+          matchIndex = i + 2
+        }
+      }
+    }
+  })
+  return setsWithComments
+}
+
+const getFtlContentWithTranslations = (ftlSetsWithComments: SetWithComments[], ftlContent: String, poContent: String) => {
+  const poMsgConcatSets = convertPoVarsToFltVars(poContent).split('\n\n')
+  
+  const translationMap = poMsgConcatSets.map((concatSet) => {
+    const poMsgSet = concatSet.split('\nmsgstr')
+    return ({
+      eng: getPoQuoteString(poMsgSet[0]),
+      translation: getPoQuoteString(poMsgSet[1]),
+    })
+  })
+
+  ftlSetsWithComments.forEach((set) => {
+    let translationFound = false
+    for (const poSet of translationMap) {
+      if (poSet.eng === set.engTranslation && poSet.translation !== "") {
+        ftlContent = ftlContent.replace(set.engTranslation, poSet.translation)
+        translationFound = true
+        break
+      }
+    }
+    // NOTE/TODO: this will set references with text between when these should be filtered out, e.g. `{ a } other copy { b }`
+    if (!translationFound && !(set.engTranslation.startsWith('{') && set.engTranslation.endsWith('}'))) {
+      // delete the line if no existing translation is found
+      const concatSetIndex = ftlContent.indexOf(set.fullString)
+      const contentBeforeSet = ftlContent.substring(0, concatSetIndex - 1)
+      const contentAfterSet = ftlContent.substring(concatSetIndex + set.fullString.length)
+      ftlContent = contentBeforeSet + contentAfterSet
+    }
+  })
+
+  return ftlContent
+}
+
+const getLangDirs = async () => {
+  const localeDirContent = await fs.readdir(localeDir)
+  // only include directories and exclude the 'templates' + 'en' directory
+  const allLangDirs: String[] = (await asyncFilter(localeDirContent, async(fileOrDirName: string) => (await fs.lstat(`${localeDir}/${fileOrDirName}`)).isDirectory())).filter(directory => directory !== 'templates' && directory !== 'en')
+  return trialRun ? [allLangDirs[0], allLangDirs[1]] : allLangDirs
+}
+
 (async () => {
   try {
-    const localeDirContent = await fs.readdir(localeDir)
-    // only include directories and exclude the 'templates' directory
-    const langDirs = (await asyncFilter(localeDirContent, async(fileOrDirName: string) => (await fs.lstat(`${localeDir}/${fileOrDirName}`)).isDirectory())).filter(directory => directory !== 'templates' && directory !== 'en')
+    const langDirs = await getLangDirs()
 
-    // comment out this for loop and change to const directory = langDirs[0] to check content before writing
     langDirs.forEach((directory) => {
-      // const directory = langDirs[0]
       const poContent = fs.readFileSync(`${localeDir}/${directory}/LC_MESSAGES/${poFile}`).toString('utf-8')
-  
-      // fs.copyFileSync(`${ftlDir}/${ftlFile}`, `${localeDir}/${directory}/${ftlFile}`)
-      // let ftlContent = fs.readFileSync(`${localeDir}/${directory}/${ftlFile}`).toString('utf-8')
-  
-      // uncomment this (and comment out two lines above) to log
-      let ftlContent = fs.readFileSync(`${ftlDir}/${ftlFile}`).toString('utf-8')
+      const ftlContent = fs.readFileSync(`${ftlDir}/${ftlFile}`).toString('utf-8')
     
-      if (poContent && ftlContent) {
-        const poMsgConcatSets = convertPoVarsToFltVars(poContent).split('\n\n')
-  
-        const translationMap = poMsgConcatSets.map((concatSet) => {
-          const poMsgSet = concatSet.split('\nmsgstr')
-          return ({
-            eng: getPoQuoteString(poMsgSet[0]),
-            translation: getPoQuoteString(poMsgSet[1]),
-          })
-        })
-  
-        // filter out blank lines
-        const ftlConcatSetsWithComments = ftlContent.match(untilNewlineWithoutSpaceRegex)!.filter(set => set !== '')
-        ftlConcatSetsWithComments.splice(0, 4) // header comment
+      // an array of every line from the ftl file except blank lines. A multi-line ftl block will be one string
+      const ftlConcatSetsWithComments = ftlContent.match(untilNewlineWithoutSpaceRegex)!.filter(set => set !== '')
+      ftlConcatSetsWithComments.splice(0, 4) // header comment
 
-        const setsWithComments: { comment: String | null, ftlId: string, engTranslation: string, set: string }[] = []
-        let matchIndex = 0
-        ftlConcatSetsWithComments.forEach((match, index) => {
-          // don't iterate over match if it's been reached in the inner loop
-          if (matchIndex <= index) {
-            if (!match.startsWith('#')) {
-              // we can grab the fluent ID/message reliably by using what's on the left/hand side of ' = ' 
-              // except for blocks where they may begin on the next line (' =')
-              const [ ftlId, engTranslation ] = match.includes(' = ') ? match.split(' = ') : match.split(' =')
-              setsWithComments.push({
-                comment: null,
-                ftlId,
-                engTranslation,
-                set: match,
-              })
-            } else {
-              let comment = match
-              for (let i = index + 1; i < ftlConcatSetsWithComments.length; i++) {
-                if (ftlConcatSetsWithComments[i].startsWith('#')) {
-                  comment += '\n' + ftlConcatSetsWithComments[i]
-                } else {
-                  const ftlIdAndTranslation = ftlConcatSetsWithComments[i]
-                  const [ ftlId, engTranslation ] = ftlIdAndTranslation.includes(' = ') ? ftlIdAndTranslation.split(' = ') : match.split(' =')
-                  setsWithComments.push({
-                    comment,
-                    ftlId,
-                    engTranslation,
-                    set: comment + '\n' + ftlIdAndTranslation,
-                  })
-                  break;
-                }
-                matchIndex = i + 2;
-              }
-            }
-          }
-        })
+      const ftlSetsWithComments = getSetsWithComments(ftlConcatSetsWithComments)
+      const ftlContentWithTranslations = getFtlContentWithTranslations(ftlSetsWithComments, ftlContent, poContent)
 
-        setsWithComments.forEach((set) => {
-
-        //   // we can grab the fluent ID/message reliably by using what's on the left/hand side of ' = ' 
-        //   // except for blocks where they may begin on the next line (' =')
-        //   const [ ftlId, engTranslation ] = concatSet.includes(' = ') ? concatSet.split(' = ') : concatSet.split(' =')
-  
-          let translationFound = false
-          for (const poSet of translationMap) {
-            if (poSet.eng === set.engTranslation && poSet.translation !== "") {
-              ftlContent = ftlContent.replace(set.engTranslation, poSet.translation)
-              translationFound = true
-              break;
-            }
-          }
-          // NOTE/TODO: this will set references with text between when these should be filtered out, e.g. `{ a } other copy { b }`
-          if (!translationFound && !(set.engTranslation.startsWith('{') && set.engTranslation.endsWith('}'))) {
-            // delete the line if no existing translation is found
-            const concatSetIndex = ftlContent.indexOf(set.set)
-            const contentBeforeSet = ftlContent.substring(0, concatSetIndex - 1)
-            const contentAfterSet = ftlContent.substring(concatSetIndex + set.set.length)
-            ftlContent = contentBeforeSet + contentAfterSet
-          }
-        })
-        // console.log('ftlContent', ftlContent + '\n')
-        fs.writeFile(`${localeDir}/${directory}/${ftlFile}`, ftlContent + '\n')
+      if (trialRun) {
+        console.log(`==========\nContent to be written to ${localeDir}/${directory}/${ftlFile}:\n==========\n`, ftlContentWithTranslations + '\n')
+      } else {
+        fs.writeFile(`${localeDir}/${directory}/${ftlFile}`, ftlContentWithTranslations + '\n')
       }
     })
-  // }
-    // uncomment this to check content before writing
-    // console.log(ftlContent)
-  
   } catch(error) {
     console.log(error)
   }

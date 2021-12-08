8ad5ab3cb3e78aa56821bd530b6ddf4f12275668
Lauren Zugai <lauren@zugai.com>
2021-12-08 12:47:08 -0600

    existing ftl functionality tweak

--- index.ts
+++ index.ts
@@ -114,7 +114,6 @@ const convertPoVarsToFltVars = (poTranslation: string) => {
 
 const getFtlSets = (ftlEntries: Entry[], termsOnly = false) => {
   const ftlSets: FtlSet[] = []
-  // const termSets: TermSet[] = additionalTermSets
   const termSets: TermSet[] = []
 
   ftlEntries.forEach((entry) => {
@@ -153,7 +152,7 @@ const getFtlSets = (ftlEntries: Entry[], termsOnly = false) => {
 
   // Some terms may contain others, e.g. "Firefox" and "Firefox accounts"
   // Since we replace strings in array order, we sort the array by string length
-  termSets.sort((a, b) => b.translation.length - a.translation.length)
+  termSets.sort((a, b) => b.translation?.length - a.translation?.length)
 
   return { ftlSets, termSets }
 }
@@ -353,7 +352,7 @@ const getLangDirs = async () => {
               newTranslatedFtl +
               '\n'
           )
-        } else if (newFtlSets) {
+        } else if (newTranslatedFtl) {
           try {
             fs.appendFile(
               `${localeDir}/${directory}/${ftlFile}`,
